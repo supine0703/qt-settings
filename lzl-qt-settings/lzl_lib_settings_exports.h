dd0d2da886114651bd0add5f8795e356f932c77b
Li Zonglin <supine0703@outlook.com>
2025-02-27 15:44:16 +0800

    [feat]: 添加信息

--- lzl-qt-settings/lzl_lib_settings_exports.h
+++ lzl-qt-settings/lzl_lib_settings_exports.h
@@ -1,3 +1,11 @@
+/**
+ * License: GPL-3.0
+ * Copyright (c) 2024 李宗霖 (Li Zonglin)
+ * Email: supine0703@outlook.com
+ * GitHub: https://github.com/supine0703
+ * Repository: qt-settings <https://github.com/supine0703/qt-settings>
+ */
+
 #ifndef ___LZL_QT_UTILS__LZL_QT_SETTINGS_EXPORTS_H__
 #define ___LZL_QT_UTILS__LZL_QT_SETTINGS_EXPORTS_H__
 
--- lzl-qt-settings/lzl_settings.cpp
+++ lzl-qt-settings/lzl_settings.cpp
@@ -1,3 +1,11 @@
+/**
+ * License: GPL-3.0
+ * Copyright (c) 2024 李宗霖 (Li Zonglin)
+ * Email: supine0703@outlook.com
+ * GitHub: https://github.com/supine0703
+ * Repository: qt-settings <https://github.com/supine0703/qt-settings>
+ */
+
 #include "lzl_settings.h"
 
 #ifndef CONFIG_INI
